986a9c160c100ff33eda9c913afd693e4fb7da9f
retiuba <ubaldo.retti@divtec.ch>
2021-01-15 13:50:33 +0100

    1er commit

---
+++ app/src/main/java/com/rettuba/speedquiz/MainActivity.java
@@ -0,0 +1,81 @@
+package com.rettuba.speedquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**Boutons centraux**/
+    private MaterialButton BT_Menu;
+    private MaterialButton BT_Rejouer;
+    /**Boutons de jeux**/
+    private MaterialButton BT_Joueur1;
+    private MaterialButton BT_Joueur2;
+    /**Compteurs**/
+    private TextView Compteur_Joueur1;
+    private TextView Compteur_Joueur2;
+    /**Noms**/
+    private TextView Nom_Joueur1;
+    private TextView Nom_Joueur2;
+    /**Questions**/
+    private TextView Questions_Joueur1;
+    private TextView Questions_Joueur2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**Boutons centrals**/
+        BT_Menu = findViewById(R.id.bt_menu);
+        BT_Rejouer = findViewById(R.id.bt_rejouer);
+        /**Boutons de jeux**/
+        BT_Joueur1 = findViewById(R.id.bt_joueur1);
+        BT_Joueur2 = findViewById(R.id.bt_joueur2);
+        /**Compteurs**/
+        Compteur_Joueur1 = findViewById(R.id.compteur_joueur1);
+        Compteur_Joueur2 = findViewById(R.id.compteur_joueur2);
+        /**Noms**/
+        Nom_Joueur1 = findViewById(R.id.nom_joueur1);
+        Nom_Joueur2 = findViewById(R.id.nom_joueur2);
+        /**Questions**/
+        Questions_Joueur1 = findViewById(R.id.questions_joueur1);
+        Questions_Joueur2 = findViewById(R.id.questions_joueur2);
+
+        /**Rendre les deux boutons "Menu" et "Rejouer" invisible**/
+        BT_Menu.setVisibility(View.GONE);
+        BT_Rejouer.setVisibility(View.GONE);
+
+        /**Lors du clique sur le bouton d'un des deux joueurs, rendre l'autre grisé**/
+        BT_Joueur1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BT_Joueur2.setEnabled(false);
+                BT_Joueur2.setBackgroundColor(Color.GRAY);
+            }
+        });
+        BT_Joueur2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BT_Joueur1.setEnabled(false);
+                BT_Joueur1.setBackgroundColor(Color.GRAY);
+            }
+        });
+
+
+
+        }
+
+
+
+    }
+
+
---
+++ app/src/main/java/com/rettuba/speedquiz/menu_speed_quiz.java
@@ -0,0 +1,106 @@
+package com.rettuba.speedquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
+
+public class menu_speed_quiz extends AppCompatActivity {
+
+    /**Champs de saisie pour le nom des joueurs**/
+    private EditText nom_joueur1;
+    private EditText nom_joueur2;
+    /**Bouton jouer**/
+    private MaterialButton bouton_jouer;
+    /**Booleens**/
+    boolean rempli_joueur1 = false;
+    boolean rempli_joueur2 = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_speed_quiz);
+
+
+
+        /**Champs de saisie pour le nom des joueurs**/
+        nom_joueur1 = findViewById(R.id.creer_joueur1);
+        nom_joueur2 = findViewById(R.id.creer_joueur2);
+        /**Bouton jouer**/
+        bouton_jouer = findViewById(R.id.jouer);
+        bouton_jouer.setEnabled(false);
+        bouton_jouer.setVisibility(View.GONE);
+
+
+
+        nom_joueur1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() >= 1){
+                    rempli_joueur1 = true;
+                    if(rempli_joueur2) {
+                        bouton_jouer.setEnabled(true);
+                        bouton_jouer.setBackgroundColor(Color.GREEN);
+                    }
+                }else{
+                    rempli_joueur1 = false;
+                    bouton_jouer.setEnabled(false);
+                    bouton_jouer.setBackgroundColor(Color.GRAY);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        nom_joueur2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() >= 1){
+                    rempli_joueur2 = true;
+                    if(rempli_joueur1) {
+                        bouton_jouer.setEnabled(true);
+                        bouton_jouer.setBackgroundColor(Color.GREEN);
+                    }
+                }else{
+                    rempli_joueur2 = false;
+                    bouton_jouer.setEnabled(false);
+                    bouton_jouer.setBackgroundColor(Color.GRAY);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+}
+
+
+
+
+
+
+
+
+
+
